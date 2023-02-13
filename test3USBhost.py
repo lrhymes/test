de9ea13eda7f196f7d21f9e257821c2001ac024f
ler <lrhymes@gmail.com>
2023-02-13 12:48:44 -0800

    tuya

--- test3USBhost.py
+++ test3USBhost.py
@@ -6,18 +6,15 @@ import struct
 import serial 
 
 
-plt.axis([-1, 11, -0.2, 1.2])
 X = np.linspace(0,9,10)
-Y = np.zeros(10)
+Y = np.zeros(10)+14000
 # #exec(open("main.py").read())
 # 
-plt.ion()
-figure, ax = plt.subplots(figsize=(10, 8))
-line1, = ax.plot(X, Y)
 
-# 
-figure2, ax2 = plt.subplots(figsize=(10, 8))
-line2, = ax2.plot(X, Y)
+figure, ax = plt.subplots(figsize=(10, 8))
+line, = ax.plot(Y)
+
+    
 
 # 
 ii = 0
@@ -29,26 +26,18 @@ s.flush()
 while True:
     s.flush()
     data = s.read(4)
-    temp = struct.unpack('>I', data)
+    temp, = struct.unpack('>I', data)
     print(temp)
     print(list(data))
     ii = ii + 1
     i = ii%10
     
-    Y[0:9] = Y[1:10]
-    Y[9] = temp[0]
+    #Y[0:9] = Y[1:10]
+    #Y[9] = temp[0]
+    Y = np.append(Y,temp)
+    line.remove()
+    line, = ax.plot(Y)
+    ax.axis([-2, np.size(Y), min(Y)-2, max(Y)+2])
     
-    line1.set_xdata(X)
-    line1.set_ydata(Y)
-    line2.set_ydata(np.flip(Y))
-    
-    ax.axis([-2, 12, min(Y)-2, max(Y)+2])
-    ax2.axis([-2, 12, min(Y)-2, max(Y)+2])
-    
-    figure.canvas.draw()
-    figure.canvas.flush_events()
-    figure2.canvas.draw()
-    figure2.canvas.flush_events()
- 
-    time.sleep(0.01)
+    plt.pause(0.1)
 
