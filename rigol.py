88af9f3b20c844c99173e15b98ede93495369407
ler <lrhymes@gmail.com>
2023-02-13 19:44:45 -0800

    rigols

--- rigol.py
+++ rigol.py
@@ -1,7 +1,9 @@
 import usbtmc
+import matplotlib.pyplot as plt
 
+#b = usbtmc.Instrument(usbtmc.list_resources()[0])
+b = usbtmc.Instrument(usbtmc.list_devices()[0])
 
-b = usbtmc.Instrument(usbtmc.list_resources()[0])
 b.open()
 b.write("*RST")
 b.write("*IDN?")
@@ -14,3 +16,12 @@ b.write(":StOP")
 b.write(":WAV:DATA?")
 
 t = b.read()
+ll = list(t)
+lll = ll[10:-1]
+d = [int.from_bytes(bytes(c,'utf-8'),'big') for c in lll]
+
+
+fig,ax = plt.subplots()
+p1, = ax.plot(d)
+plt.pause(0.1)
+
